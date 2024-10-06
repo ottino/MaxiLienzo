3e274d1431015c0fc78de6566e6e2395e65edff7
Usuario <Usuario@DESKTOP-6H3N4LJ>
2024-10-06 00:31:01 -0300

    Agregar archivos de proyecto.

---
+++ MaxiLienzo/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MaxiLienzo
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Evento para cerrar la ventana
+        private void CloseWindow(object sender, MouseButtonEventArgs e)
+        {
+            this.Close(); // Cierra la ventana
+        }
+
+        // Evento para minimizar la ventana
+        private void MinimizeWindow(object sender, MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized; // Minimiza la ventana
+        }
+
+        // Evento para maximizar/restaurar la ventana
+        private void MaximizeWindow(object sender, MouseButtonEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+                this.WindowState = WindowState.Maximized; // Maximiza la ventana
+            }
+            else
+            {
+                this.WindowState = WindowState.Normal; // Restaura el tamaño original
+            }
+        }
+    }
+}
